1699639b6561908544c66858ffbdaf765b644edc
Federico Ceratto <federico.ceratto@gmail.com>
2010-04-05 15:47:28 +0100

    debian/: files edited.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(python-nmap):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="python-nmap" command="/usr/bin/python-nmap"
--- debian/python-nmap.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the python-nmap package
-#
-0 4	* * *	root	[ -x /usr/bin/python-nmap_maintenance ] && /usr/bin/python-nmap_maintenance
